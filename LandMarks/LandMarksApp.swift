f0920a458f186448d5722f6fbfe6cde8b656a930
purnachandraobulasetty <purnachandra.tech@gmail.com>
2024-07-17 11:01:57 +0530

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by Purnachandra on 17/07/24.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
